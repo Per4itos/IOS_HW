e85628a559819d9ed3e4ea2393b0ccebc984a1be
Per4itos <101799073+Per4itos@users.noreply.github.com>
2023-09-16 13:07:59 +0300

    fixed_wh.1

--- FavoriteNewsPick/CoreDataManager.swift
+++ FavoriteNewsPick/CoreDataManager.swift
@@ -5,4 +5,69 @@
 //  Created by Адхам Тангиров on 15.09.2023.
 //
 
-import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    init(){
+        
+        reloadNews()
+    }
+    
+   lazy var persistentContainer: NSPersistentContainer = {
+            
+        let container = NSPersistentContainer(name: "FavoriteNewsData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            print("storeDescription = \(storeDescription)")
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    } ()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    var news = [News]()
+    
+    
+    func reloadNews() {
+        let request = News.fetchRequest()
+        
+        let news = (try? persistentContainer.viewContext.fetch(request)) ?? []
+        
+        self.news = news
+    }
+    
+    func createNews(title: String, image: String, text: String, likes: String, views: String) {
+        
+        let news = News(context: persistentContainer.viewContext)
+        
+        news.title = title
+        news.image = image
+        news.text = text
+        news.likes = likes
+        news.views = views
+        
+        saveContext()
+        reloadNews()
+    }
+    
+    func deleteNews(news: News) {
+        persistentContainer.viewContext.delete(news)
+        reloadNews()
+    }
+}
--- FavoriteNewsPick/FavoriteViewController.swift
+++ FavoriteNewsPick/FavoriteViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FavoriteTableView.swift
+//  FavoriteViewController.swift
 //  Naigation_HW
 //
 //  Created by Адхам Тангиров on 15.09.2023.
@@ -8,4 +8,66 @@
 import UIKit
 import CoreData
 
-Class 
+class FavoriteViewController: UIViewController {
+    
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        tableView.estimatedRowHeight = 55
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(self.tableView)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshTable), name: NSNotification.Name(rawValue: "newsUpdate"), object: nil)
+
+        setupView()
+    }
+    
+    private func setupView() {
+        
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 55),
+            self.tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+    }
+
+@objc func refreshTable() {
+    tableView.reloadData()
+}
+    
+}
+
+
+extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CoreDataManager.shared.news.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! NewsTableViewCell
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        cell.setup(with: CoreDataManager.shared.news[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
+    }
+}
+
